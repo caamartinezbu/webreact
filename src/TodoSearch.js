f63b324792bca4a8cd6f075c75ba670bb4fe1299
betocode <carlosmartinez622@gmail.com>
2022-08-24 01:46:00 -0500

    agregando estados o hooks a ToDoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,18 +3,23 @@ import './TodoSearch.css';
 
 function TodoSearch(){
 
+    const [searchValue, setSearchValue]= React.useState('');
+
     const onSearchValueChange = (event)=>{
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-return(
+return[
     <input 
     className="TodoSearch" 
     placeholder="Cebolla"
+    value={searchValue}
     onChange={onSearchValueChange}
-    />
+    />,
+    <p>{searchValue}</p>
 
-);
+  ];
 
 }
 
